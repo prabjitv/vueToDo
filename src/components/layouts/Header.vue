4b6d10a4dd9201b781e498d0c98089f5540018b1
Prabjit <prabjitv@gmail.com>
2021-01-09 23:19:32 -0700

    header, delete, and adding of Todo implemented

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h1>To Do List</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+a {
+  color: #ffffff;
+  padding-right: 5px;
+}
+</style>
